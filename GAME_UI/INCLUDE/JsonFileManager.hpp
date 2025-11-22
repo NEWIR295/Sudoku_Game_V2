2c7d5d8ee852b9497ab19963e87331a4a3009e7e
NEWIR529 <newir295@gmail.com>
2025-11-22 07:54:33 +0200

    upload GameJsonFileManager Class

---
+++ GAME_UI/INCLUDE/JsonFileManager.hpp
@@ -0,0 +1,24 @@
+#ifndef JSON_FILE_MANGER_HPP
+#define JSON_FILE_MANGER_HPP
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+class GameJsonFileManager
+{
+
+public:
+    GameJsonFileManager(std::string filename = "Default_Puzzle");
+    bool readJsonFile(std::vector<std::vector<int>> &boardData);
+    bool writeJsonFile(const std::vector<std::vector<int>> &boardData);
+
+private:
+    std::string filename;
+    std::ofstream outputFile;
+    std::ifstream inputFile;
+    nlohmann::json jsonFile;
+};
+
+#endif
